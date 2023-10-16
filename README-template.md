bb326f910309835b106d5c214dbfb27e78b6b4dc
godswillnwabu <godswillnwabeke50@gmail.com>
2023-10-16 13:59:21 +0100

    First Commit

---
+++ README-template.md
@@ -0,0 +1,59 @@
+# Frontend Mentor - News homepage solution
+
+This is a solution to the [News homepage challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/news-homepage-H6SWTa1MFl). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+- Live Site URL: [See the live site URL here](https://godswillnwabu.github.io/frontendmentor-challenge-cardForm/)
+
+## Table of contents
+
+- [Overview](#overview)
+- [The challenge](#the-challenge)
+- [My challenge](#my-challenges)
+- [Screenshot](#screenshot)
+- [Built with](#built-with)
+- [What I learned](#what-i-learned)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+I made this form project to understand more about form authentications; it was a little bit complicated but was actually fun at the end. I learnt alot.
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- **Bonus**: Toggle the mobile menu (requires some JavaScript)
+
+### My challenges
+* I had some couple of styling issues but i got them fixed as i played along with them.
+* Getting the Card details and replacing them with the form inputs got me confusing after showing a undefined value; so i went on to study more about HTML DOM which helped me to complete my task.
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Vanilla JavaScript
+
+### What I learned
+
+Learning about the JavaScript innerHTML, textContent, and innerText made me understand how elements could be placed in documents. 
+
+## Author
+
+- Website - [Github](https://github.com/godswillnwabu)
+- Frontend Mentor - [@godswillnwabu](https://www.frontendmentor.io/profile/godswillnwabu)
+- Twitter - [@GodswillNwabeke](https://www.twitter.com/GodswillNwabeke)
+
+
+## Acknowledgments
+
+I want to thank everyone who their projects and hardwork have been a sort of initiative to me.
+
---
+++ main.js
@@ -0,0 +1,105 @@
+// // Declaring variables for the input errors
+var nameError = document.getElementById("name-error");
+var numberError = document.getElementById("number-error");
+var dateError = document.getElementById("date-error");
+var cvcError = document.getElementById("cvc-error");
+var submitError = document.getElementById("submit-error");
+var inputError = document.querySelector(".input-error");
+
+// Declaring variables for the card details
+var cardName = document.getElementById("card-name");
+var cardNumber = document.getElementById("card-number");
+var cardDate = document.getElementById("card-date");
+var cardCvc = document.getElementById("card-cvc");
+
+
+function validateName() {
+    var name = document.getElementById("fname").value;
+
+    if (name.length == 0) {
+        inputError.classList.add("invalid");
+        nameError.innerHTML = "name is blank";
+        return false;
+    }
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        inputError.classList.add("invalid");
+        nameError.innerHTML = "fullname is needed";
+        return false;
+    }
+    nameError.innerHTML = "<i class='fa-solid fa-circle-check'></i>";
+    inputError.classList.remove("invalid");
+    cardName.textContent = name.toUpperCase();
+    return true;
+}
+
+function validateNumber() {
+    var number = document.getElementById("fnumber").value; 
+    
+    if (number == "") {
+        // inputError.classList.add("invalid")
+        numberError.innerHTML = "please input numbers";
+        return false;
+    }
+    if (number.length !== 16) {
+        // inputError.classList.add("invalid")
+        numberError.innerHTML = "numbers should be 16 digits";
+        return false;
+    }
+    if (!number.match(/^[0-9]{16}$/)) {
+        // inputError.classList.add("invalid")
+        numberError.innerHTML = "only digits please";
+        return false;
+    }
+    numberError.innerHTML = "<i class='fa-solid fa-circle-check'></i>";
+    // inputError.classList.remove("invalid")
+    cardNumber.textContent = number.replace(/\d{4}(?=.)/g, '$& ');
+    return true;
+}
+
+function validateDate() {
+    var month = document.getElementById("fmonth").value;
+    var year = document.getElementById("fyear").value;
+
+    if (month == "" || year == "") {
+        // inputError.classList.add("invalid")
+        dateError.innerHTML = "can't be blank";
+        return false;
+    }
+    dateError.innerHTML = "<i class='fa-solid fa-circle-check'></i>";
+    // inputError.classList.remove("invalid")
+    cardDate.textContent = month + "/" + year;
+    return true;
+}
+
+function validateCvc() {
+    var cvc = document.getElementById("fcvc").value;
+
+    if (cvc == "") {
+        // inputError.classList.add("invalid")
+        cvcError.innerHTML = "can't be blank";
+        return false;
+    }
+    if (cvc.length !== 3) {
+        // inputError.classList.add("invalid")
+        cvcError.innerHTML = "3 digits are needed";
+        return false;
+    }
+    cvcError.innerHTML = "<i class='fa-solid fa-circle-check'></i>";
+    // inputError.classList.remove("invalid")
+    cardCvc.textContent = cvc;
+    return true;
+}
+
+function validateForm() {
+    var showCompleted = document.getElementById("complete");
+    var myForm = document.getElementById("my-form");
+
+    if (!validateName() || !validateNumber() || !validateDate() || !validateCvc()) {
+        submitError.style.display = "block";
+        submitError.innerHTML = "please fix the error warnings";
+        setTimeout(function(){submitError.style.display = "none";}, 3000);
+        return false;
+    }
+    myForm.style.display = "none";
+    showCompleted.style.display = "block";
+}
